a13048c32af9fe82731d006290d0210b2445e4f2
Asem <asem.mahmod@gmail.com>
2020-03-19 12:18:10 +0200

    added build folder

---
+++ build/precache-manifest.247e3d4e7c71cfb685f594e07c2d0561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46de71c76511eee6ee3c460eb41408dc",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "81d36cb6d3840bdc10f5",
+    "url": "/lights-out/static/css/main.2990d2c0.chunk.css"
+  },
+  {
+    "revision": "9f593cfb17bf92676bd3",
+    "url": "/lights-out/static/js/2.b78045f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lights-out/static/js/2.b78045f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81d36cb6d3840bdc10f5",
+    "url": "/lights-out/static/js/main.43e8c854.chunk.js"
+  },
+  {
+    "revision": "b1ce66f302e11e60fc20",
+    "url": "/lights-out/static/js/runtime-main.88fcecc8.js"
+  }
+]);
